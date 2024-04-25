2f7e992089c8d8a1ceecfdd1e9d43de8ff470107
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-25 15:30:57 +0200

    update

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,10 +1,58 @@
 import User from "../models/user.model.js";
-// import createError from "../utils/createError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+// import createError from "../utils/createError.js";
 
-export const register = async (req, res, next) => {};
+export const register = async (req, res) => {
+  try {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 12);
+    const newUser = await new User({
+      ...req.body,
+      password: hashedPassword,
+    }).save();
 
-export const login = async (req, res, next) => {};
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export const logout = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json("Wrong password or username!");
+    }
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(401).json("Wrong password or username!");
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
+};
