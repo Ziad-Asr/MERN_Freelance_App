26d1a28501a85169c3881b4bb312c7b1d85c8803
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-25 13:49:12 +0200

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 
+dotenv.config();
 const app = express();
+app.use(cors());
 
-app.listen(8800, () => {
-  console.log("Server is running on port 8800!");
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_KEY);
+    console.log(`DB is connected!`);
+  } catch (err) {
+    console.error("Error connecting to database:", err);
+  }
+};
+
+app.listen(process.env.PORT, () => {
+  connect();
+  console.log(`Server is running on port ${process.env.PORT}!`);
 });
