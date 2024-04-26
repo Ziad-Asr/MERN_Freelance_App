26ec93d12f552a796b000d5234a22c5719e95244
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-26 16:03:19 +0300

    update

--- Frontend/src/utils/upload.js
+++ Frontend/src/utils/upload.js
@@ -3,13 +3,16 @@ import axios from "axios";
 const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
-  data.append("upload_preset", "fiverr");
+  data.append("upload_preset", "MERN_Freelance_App");
 
   try {
-    const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+    const res = await axios.post(
+      "https://api.cloudinary.com/v1_1/dxzp97kmr/image/upload",
+      data
+    );
 
-    const { url } = res.data;
-    return url;
+    const { secure_url } = res.data;
+    return secure_url;
   } catch (err) {
     console.log(err);
   }
