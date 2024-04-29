c1e174d85805506f254890c444c4bf43582d2902
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-29 14:36:49 +0300

    update

--- Backend/routes/review.route.js
+++ Backend/routes/review.route.js
@@ -1,15 +1,15 @@
 import express from "express";
-// import { verifyToken } from "../middleware/jwt.js";
+import { verifyToken } from "../middleware/jwt.js";
 import {
   createReview,
-  getReviews,
+  getGigReviews,
   deleteReview,
 } from "../controllers/review.controller.js";
 
 const router = express.Router();
 
-// router.post("/", verifyToken, createReview);
-router.get("/:gigId", getReviews);
+router.post("/", verifyToken, createReview);
+router.get("/:gigId", getGigReviews);
 router.delete("/:id", deleteReview);
 
 export default router;
--- Frontend/src/components/review/Review.jsx
+++ Frontend/src/components/review/Review.jsx
@@ -1,7 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import newRequest from "../../utils/newRequest";
 
-const Review = () => {
-  return <div>Review</div>;
+const Review = ({ review }) => {
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: [review.userId],
+    queryFn: () =>
+      newRequest.get(`/users/${review.userId}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  return (
+    <div className="item">
+      {isLoading ? (
+        "Loading"
+      ) : error ? (
+        "Something went wrong"
+      ) : (
+        <div className="user">
+          <img className="pp" src={data.img} alt="" />
+          <div className="info">
+            <span>{data.username}</span>
+            <div className="country">
+              <span>{data.country}</span>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="stars">
+        {Array(review?.star)
+          .fill()
+          .map((item, i) => (
+            <img src="/img/star.png" alt="" key={i} />
+          ))}
+        <span>{review?.star}</span>
+      </div>
+      <p>{review?.desc}</p>
+      <div className="helpful">
+        <span>Helpful?</span>
+        <img src="/img/like.png" alt="" />
+        <span>Yes</span>
+        <img src="/img/dislike.png" alt="" />
+        <span>No</span>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
