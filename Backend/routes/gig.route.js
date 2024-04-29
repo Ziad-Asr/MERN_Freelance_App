e51d75dadcf80fd6a7c6920034896fe782f5f821
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-29 11:53:42 +0300

    update

--- Backend/routes/gig.route.js
+++ Backend/routes/gig.route.js
@@ -5,12 +5,11 @@ import {
   getGig,
   getGigs,
 } from "../controllers/gig.controller.js";
-import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, createGig);
-router.delete("/:id", verifyToken, deleteGig);
+router.post("/", createGig);
+router.delete("/:id", deleteGig);
 router.get("/single/:id", getGig);
 router.get("/", getGigs);
 
