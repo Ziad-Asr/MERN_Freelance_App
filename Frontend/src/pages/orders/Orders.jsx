a48cbc190abcba56e098ac76bbf51f2ca2b1e0da
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-30 19:20:46 +0300

    update

--- Frontend/src/pages/orders/Orders.jsx
+++ Frontend/src/pages/orders/Orders.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Orders.scss";
 import { useQuery } from "@tanstack/react-query";
-import newRequest from "./../../utils/newRequest";
+import newRequest from "../../utils/newRequest";
 
 const Orders = () => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
+  const navigate = useNavigate();
   const { isLoading, error, data } = useQuery({
     queryKey: ["orders"],
     queryFn: () =>
@@ -15,15 +16,29 @@ const Orders = () => {
       }),
   });
 
-  console.log("orders");
-  console.log(data);
+  const handleContact = async (order) => {
+    const sellerId = order.sellerId;
+    const buyerId = order.buyerId;
+    const id = sellerId + buyerId;
 
+    try {
+      const res = await newRequest.get(`/conversations/single/${id}`);
+      navigate(`/message/${res.data.id}`);
+    } catch (err) {
+      if (err.response.status === 404) {
+        const res = await newRequest.post(`/conversations/`, {
+          to: currentUser.seller ? buyerId : sellerId,
+        });
+        navigate(`/message/${res.data.id}`);
+      }
+    }
+  };
   return (
     <div className="orders">
       {isLoading ? (
-        "Loading"
+        "loading"
       ) : error ? (
-        "Something went wrong"
+        "error"
       ) : (
         <div className="container">
           <div className="title">
@@ -36,21 +51,23 @@ const Orders = () => {
               <th>Price</th>
               <th>Contact</th>
             </tr>
-
-            {data.map((order, i) => {
-              return (
-                <tr key={i}>
-                  <td>
-                    <img className="image" src={order.img} alt="" />
-                  </td>
-                  <td>{order.title}</td>
-                  <td>{order.price}</td>
-                  <td>
-                    <img className="message" src="./img/message.png" alt="" />
-                  </td>
-                </tr>
-              );
-            })}
+            {data.map((order) => (
+              <tr key={order._id}>
+                <td>
+                  <img className="image" src={order.img} alt="" />
+                </td>
+                <td>{order.title}</td>
+                <td>{order.price}</td>
+                <td>
+                  <img
+                    className="message"
+                    src="./img/message.png"
+                    alt=""
+                    onClick={() => handleContact(order)}
+                  />
+                </td>
+              </tr>
+            ))}
           </table>
         </div>
       )}
