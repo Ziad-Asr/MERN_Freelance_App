8c988c50c256e82dc7bf0faab184535c660aa2a8
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-04-30 17:57:18 +0300

    update

--- Backend/routes/conversation.route.js
+++ Backend/routes/conversation.route.js
@@ -5,13 +5,13 @@ import {
   getSingleConversation,
   updateConversation,
 } from "../controllers/conversation.controller.js";
-// import { verifyToken } from "../middleware/jwt.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-// router.get("/", verifyToken, getConversations);
-// router.post("/", verifyToken, createConversation);
-// router.get("/single/:id", verifyToken, getSingleConversation);
-// router.put("/:id", verifyToken, updateConversation);
+router.get("/", verifyToken, getConversations);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.post("/", verifyToken, createConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 export default router;
