4cc18979e996b2ed5639478cd744847af7e10b6c
Ziad-Asr <ziadelsayedasr@gmail.com>
2024-05-01 01:46:50 +0300

    update

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -2,8 +2,36 @@ import createError from "../utils/createError.js";
 import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
 import Stripe from "stripe";
-import orderModel from "../models/order.model.js";
-import gigModel from "../models/gig.model.js";
+
+export const intent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
+
+  const gig = await Gig.findById(req.params.id);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};
 
 export const getOrders = async (req, res, next) => {
   try {
@@ -36,59 +64,3 @@ export const confirm = async (req, res, next) => {
     next(err);
   }
 };
-
-export const intent = async (req, res, next) => {
-  const stripe = new Stripe(process.env.STRIPE);
-
-  const gig = await Gig.findById(req.params.id);
-
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: gig.price * 100,
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  const newOrder = new Order({
-    gigId: gig._id,
-    img: gig.cover,
-    title: gig.title,
-    buyerId: req.userId,
-    sellerId: gig.userId,
-    price: gig.price,
-    payment_intent: paymentIntent.id,
-  });
-
-  await newOrder.save();
-
-  res.status(200).send({
-    clientSecret: paymentIntent.client_secret,
-  });
-};
-
-// export const createOrder = async (req, res, next) => {
-//   try {
-//     const gig = await gigModel.findById(req.params.gigId);
-
-//     if (!gig) {
-//       return res.status(404).send("Gig not found");
-//     }
-
-//     const newOrder = {
-//       gigId: gig._id,
-//       sellerId: gig.userId,
-//       price: gig.price,
-//       img: gig.cover,
-//       title: gig.title,
-//       buyerId: req.userId,
-//       payment_intent: "Temp",
-//     };
-
-//     await orderModel.create(newOrder);
-//     res.status(200).send("Order created successfully!");
-//   } catch (err) {
-//     console.error("Error creating order:", err);
-//     next(err);
-//   }
-// };
